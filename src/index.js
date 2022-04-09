68c070d9e089a90d7a05ce2ad18a85433268ebd4
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-09 19:35:28 +0530

    wrap App in Router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
