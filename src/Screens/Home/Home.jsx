efbad2038052698e9d2b5a7df1543b54caf05cda
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-10 16:52:35 +0530

    add home component

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,5 +1,25 @@
+import "./Home.css";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main className="main_home px_2 py_2">
+      <div className="home_text txt_center flex_col flex_align__center">
+        <h1 className="h1">
+          Stop procrastinating and start doing with <span>stopphur</span>
+        </h1>
+        <Link to="/tasks" className="btn btn_get__started px_6 py_3 btn_link">
+          Get Started &rarr;
+        </Link>
+      </div>
+      <div className="home_image">
+        <img
+          src="/images/stopphur home.svg"
+          alt="clock"
+          className="img_responsive"
+        />
+      </div>
+    </main>
+  );
 };
 
 export { Home };
