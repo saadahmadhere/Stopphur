c3a4fd8937dca1165e8833a44ee79e2e3f57e27b
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-13 17:53:52 +0530

    create task component

--- src/Screens/Tasks/Tasks.jsx
+++ src/Screens/Tasks/Tasks.jsx
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import "./Tasks.css";
+import { SingleTask } from "./SingleTask";
+import { Modal } from "../../Components";
+import { useFixSlider } from "../../Hooks";
+import { useTasks } from "../../Contexts";
+
 const Tasks = () => {
-  return <div>Tasks</div>;
+  const [showModal, setShowModal] = useState(false);
+  const { tasks } = useTasks();
+  useFixSlider(showModal);
+  return (
+    <main className="tasks_bg__light">
+      {showModal && <Modal setShowModal={setShowModal} showModal={showModal} />}
+      <div className="main_tasks">
+        <section className="tasks_welcome__text">
+          <h1 className="h1">Welcome back, User</h1>
+          <h3 className="h3">You have 3 task(s) remaining for the day!</h3>
+        </section>
+        <section className="tasks_todo py_10 px_12 mt_10">
+          <div className="add_tasks">
+            <h2 className="h2">Tasks Todo</h2>
+            <button className="btn btn_todo" onClick={() => setShowModal(true)}>
+              <i className="fas fa-plus-circle fa-3x "></i>
+            </button>
+          </div>
+          <div className="tasks_list px_6 mt_8">
+            {tasks.map((task) => (
+              <SingleTask title={task.title} id={task._id} key={task._id} />
+            ))}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export { Tasks };
