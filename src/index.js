f32033785fa472a18292336e069a2a18d2da3388
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-13 17:52:15 +0530

    create task context

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { TasksProvider } from "./Contexts";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <Router>
-    <App />
+    <TasksProvider>
+      <App />
+    </TasksProvider>
   </Router>
 );
