da67e99551aaa84b1621c4d084afb3a30283e7c1
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2022-04-15 02:54:51 +0530

    update SingleTask for editing feature

--- src/Screens/Tasks/SingleTask.jsx
+++ src/Screens/Tasks/SingleTask.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import { useTasks } from "../../Contexts";
+import { Modal } from "../../Components";
 
-const SingleTask = ({ title, id }) => {
+const SingleTask = ({ task }) => {
   const { setTasks } = useTasks();
+  const { id, title } = task;
+  const [isGettingEdited, setIsGettingEdited] = useState(false);
 
   const deleteTaskHandler = () => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task._id !== id));
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   };
 
   return (
     <div className="task flex px_5 py_2 mb_4">
+      {isGettingEdited && (
+        <Modal
+          taskToBeEdited={task}
+          isGettingEdited={isGettingEdited}
+          setIsGettingEdited={setIsGettingEdited}
+        />
+      )}
+
       <h4 className="h4">{title}</h4>
       <div className="task_buttons flex ">
-        <button className="btn btn_edit__task ml_2">
+        <button
+          className="btn btn_edit__task ml_2"
+          onClick={() => setIsGettingEdited(true)}>
           <i className="far fa-edit fa-lg"></i>
         </button>
         <button
